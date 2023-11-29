f2b20fd13b6b3451982f721905a0a47c5ba39a1a
Taweria <elodieali.pro@gmail.com>
2023-11-29 12:10:10 +0100

    Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -16,7 +16,24 @@ function ProfilePage() {
         <div>
             {loading && <Loading />}
             <Header />
-            <h2 className="Working"> Working on it... </h2>
+            <div className="Profile-div">
+                <div className="Profile-img-div">
+                    <img src="avatar.jfif" alt="profile-img" className="Profile-img" />
+                </div>
+                <div className="Profile-info-div">
+                    <h2 className="Profile-name">Jane Doe</h2>
+                    <h2 className="Profile-city"> New York, United States</h2>
+                </div>
+                <div className="Profile-settings-div">
+                    <ul>
+                        <li className="Profile-favorites"> <img src="heart.svg" alt="heart" className="icons" />My Favorite List</li>
+                        <li className="Profile-edit"> <img src="pencil.svg" alt="pencil" className="icons" />Edit Profile</li>
+                        <li className="Profile-message"> <img src="chat.svg" alt="lock" className="icons" />Messages</li>
+                        <li className="Profile-settings"> <img src="settings.svg" alt="settings" className="icons" />Settings</li>
+                        <li className="Profile-logout"> <img src="exit.svg" alt="exit" className="icons" />Logout</li>
+                    </ul>
+                </div>
+            </div>
             <Footer />
         </div>
     );
